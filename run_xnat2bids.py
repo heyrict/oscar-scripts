db0fb4643de2f115676db0a4ddbc78d769a418db
Ford McDonald <fmcdona4@node1103.oscar.ccv.brown.edu>
2023-03-07 13:31:34 -0500

    feat: append xnat2bids parameters to arglist

--- run_xnat2bids.py
+++ run_xnat2bids.py
@@ -7,13 +7,78 @@ import shutil
 import subprocess
 from toml import load
 
-# Instantiate argument parser
+def extractParams(param, value):
+    arg = []
+    for v in value:
+        arg.append(f"--{param} {v}")
+    return ' '.join(arg)
+
+
+def compileArgumentList(arg_dict):
+    """Create command line argument list from TOML dictionary."""
+    x2b_param_list = []
+    slurm_param_list = []
+
+    param_lists = ["includeseq", "skipseq"]
+
+    # Fetch user credentials 
+    user = input('Enter Username: ')
+    password = getpass('Enter Password: ')
+    x2b_param_list.append(f"--user {user}")
+    x2b_param_list.append(f"--pass {password}")
+
+    # Compile list of appended arguments
+    for section_name, section_dict in arg_dict.items():
+        if section_name == "xnat2bids-args":
+            for param, value in section_dict.items():
+                if value != "" and value is not None:
+                    # Set {session} as first parameter
+                    if param == "session":
+                        arg = f"{value}"
+                        x2b_param_list.insert(0,arg)
+                    # Set {bids_root} as second parameter
+                    elif param == "bids_root":
+                        data_dir = arg_dict["xnat2bids-args"]["data_dir"]
+                        arg = f"{data_dir}/shared/bids-export/{user}"
+                        x2b_param_list.insert(1, arg)
+                    # Skip data_dir. Not a parameter to xnat2bids
+                    elif param == "data_dir":
+                        continue
+                    # If verbose is equal to 1, set flag.
+                    elif param == "verbose":
+                        arg = f"--{param}"
+                        if value == 1: x2b_param_list.append(arg)
+                    # If overwrite is equal to true, set flag.
+                    elif param == "overwrite":
+                        arg = f"--{param}"
+                        if value == True: x2b_param_list.append(arg) 
+                    # Extract parameters from include / skip lists
+                    elif param in param_lists:
+                        arg = extractParams(param, value)
+                        x2b_param_list.append(arg)
+                    # Other arguments follow --param value format.
+                    else:
+                        arg = f"--{param} {value}"
+                        x2b_param_list.append(arg)
+
+        elif section_name == "sbatch-args":
+            for param, value in section_dict.items():
+                if value != "" and value is not None:
+                    arg = f"--{param} {value}"
+                    slurm_param_list.append(arg)
+
+
+
+    return x2b_param_list, slurm_param_list, user
+
+
+# Instantiate argument parserß
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", help="path to user config")
 args = parser.parse_args()
 
 # Load default config file into dictionary
-default_params = load('/gpfs/data/bnc/scripts/x2b_config.toml')
+default_params = load('/gpfs/data/bnc/scripts/x2b_default_config.toml')
 
 # Set arglist. If user provides config, merge dictionaries.
 if args.config is None:
@@ -23,49 +88,24 @@ else:
     default_params.update(user_params)
     arglist = user_params
 
-# Extract arglist from dictionary
-for key in arglist:
-
-    # Populate sbatch arguments
-    if (key == "sbatch-args"):
-        time = arglist[key]['time'] 
-        mem = arglist[key]['mem'] 
-        nodes = arglist[key]['nodes'] 
-        cpus = arglist[key]['cpus'] 
-        job =  arglist[key]['job'] 
-        output = arglist[key]['output'] 
-        email = arglist[key]['email']
-
-    # Populate xnat2bids arguments
-    if(key == "xnat2bids-args"):
-        version = arglist[key]['version'] 
-        data_dir = arglist[key]['data_dir'] 
-        bids_root = arglist[key]['bids_root'] 
-        session = arglist[key]['session'] 
+# Fetch compiled xnat2bids abd slurm parameter lists
+x2b_param_list, slurm_param_list, user = compileArgumentList(arglist)
 
 # Fetch latest version if not provided
-if(version == ""):
+if(arglist['xnat2bids-args']['version'] == ""):
     list_of_versions = glob.glob('/gpfs/data/bnc/simgs/brownbnc/*') 
     latest_version = max(list_of_versions, key=os.path.getctime)
     version = latest_version.split('-')[-1].replace('.sif', '')
 
-# Fetch user credentials 
-user = input('Enter Username: ')
-password = getpass('Enter Password: ')
-
-# Define bids root directory
-if(bids_root == ""):
-    bids_root = f"{data_dir}/shared/bids-export/{user}"
-
 # Define singularity image 
 simg=f"/gpfs/data/bnc/simgs/brownbnc/xnat-tools-{version}.sif"
 
+# Define path to bnc data directory
+data_dir = arglist['xnat2bids-args']['data_dir']
+
 # Run xnat2bids via srun
 subprocess.run(shlex.split(
-    f"srun -t {time} --mem {mem} -N {nodes} -c {cpus} -J {job} \
-        -o {output} --mail-type=ALL --mail-user {email} \
+    f"srun {' '.join(slurm_param_list)} \
     singularity exec --contain -B {data_dir} {simg} \
-    xnat2bids {session} {bids_root} -u {user} -p {password}"
+    xnat2bids {' '.join(x2b_param_list)}"
 ))
-
-
